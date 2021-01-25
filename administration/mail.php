bb308a2d572f16a501b08b1e6a4f1b8fbc917bb4
Sealk <55085328+Sealkounet@users.noreply.github.com>
2021-01-25 12:25:16 +0100

    Update mail.php

--- administration/mail.php
+++ administration/mail.php
@@ -18,7 +18,7 @@ try {
     $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
     $mail->Username   = 'sealk91@gmail.com';                     // SMTP username
-    $mail->Password   = 'Sealk6628';                               // SMTP password
+    $mail->Password   = '';                               // SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
     $mail->Port       = 587;                                    // TCP port to connect to
 
